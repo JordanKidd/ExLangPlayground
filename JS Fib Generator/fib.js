8a268ed0e355923da4e89c0e72ab6449a836f0dc
Jordan <jordan.m.kidd@wmich.edu>
2014-07-09 01:24:08 -0400

    Adding JS Fib Generator

---
+++ JS Fib Generator/fib.js
@@ -0,0 +1,13 @@
+function fib(num) {
+    if(num == 1 || num == 0)
+        return num;
+    else
+        return fib(num - 1) + fib(num - 2);
+}
+
+function popup() {
+    var userNum = window.prompt("Please enter a Fibonacci sequence number: ", "");
+    var result = fib(userNum);
+    var message =  "The " + userNum + " element in the Fibonacci sequence is: " + result;
+    document.getElementById("displayRes").innerHTML = message;
+}
