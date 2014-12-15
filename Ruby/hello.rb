9cfcdfb4b2001146bc660abf1e271b0ecbaafa3d
Jordan <jordan.m.kidd@wmich.edu>
2014-12-14 21:56:10 -0500

    Clean up

--- Ruby/hello.rb
+++ Ruby/hello.rb
@@ -3,28 +3,34 @@
 #Global variables start with $
 $global_var = 2.17
 
+
 #Arguments to ruby vm
 arg1, arg2, arg3 = ARGV
 puts "\nDid you pass any arguments to this script?"
-puts "If so, they are: #{arg1} #{arg2} #{arg3}\n"
+puts "--- If so, the first 3 are: #{arg1} #{arg2} #{arg3}\n"
 
 
 #This is a Ruby comment!
 puts "Hello world"
 puts "Include variables in string by: #{4+5}"
+#expressions can be included inside the #{} of the string
 
 
 #Using variables:
 pi = 3.14159
 answer = 42
+big = 123_460   #_ is ignored, for ease of use with large numbers
 hello = "hi"
 binary = true
 x, y, z = 1, 2, 3
 a = [101, 202, 303]
+range = 0..9
+hashmap = { 'green' => true, 'red' => false }
 CONST = "I can't be changed. Capital letter starts consts."
 CONST = "test?" #WARNING!!
 puts "Their values: #{pi}, #{answer}, #{hello}, #{y}."
 puts "String " + "Concatenation"
+puts "Key-Value pair structure in Ruby: " + hashmap['green'].to_s()
 puts "." * 15
 
 
@@ -40,12 +46,14 @@ f = File.new("test.txt", "w")
 f.write("This is a new file, made with Ruby!")
 f.close
 
+
 #Read file
 f = open("test.txt" , "r")
 contains = f.readline
 puts contains
 f.close
 #FILES -----------------------------------------
+
 
 #FUNCTIONS -------------------------------------
 def functionOne()
@@ -66,6 +74,7 @@ functionOne()
 functionTwo("I'm two!")
 functionThree("I", "have", "stuff!")
 
+
 #LOOPS ----------------------------------------
 i = 0
 while i < 5 do
@@ -79,18 +88,45 @@ end
 
 
 #CLASSES -------------------------------------
+#class name must be CONSTANT (therefore start with a capital):
 class Person
     #This is a person class, which holds a person's name
-    @@class_var = "Available across ALL persons"
+    @@person_count = 0
+    #instance variables use a single '@', held only by each object
     @name = ""
+
     def initialize(name)
+        @@person_count += 1
         @name = name
     end
 
+    def howManyPersons()
+        puts "There are #{@@person_count} Person objects in memory.\n"
+    end
+
     def sayName()
+        #@ must be included when referencing the variable
         puts "My name is #{@name}.\n"
     end
 end
 
-jordan = Person.new("Jordan")
-jordan.sayName
+#class inheritence is Sub < Super
+class Male < Person
+    def goPee()
+        puts "My name is #{@name} and I stand to pee."
+    end
+end
+
+class Female < Person
+    def goPee()
+        puts "My name is #{@name} and I sit to pee."
+    end
+end
+
+
+jordan = Male.new("Jordan")
+jordan.sayName()
+jordan.howManyPersons()
+jordan.goPee()
+puts "\n"
+#--------------------------------------------------
